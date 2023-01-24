46b112ee6d82f97aabe1eeb55d42aacc6501c2f6
mcgidoug <mcgidoug@gmail.com>
2023-01-24 15:37:27 -0600

    added video background file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import videoBg from "../assets/wallpaper1.mp4";
 
 const Main = () => {
   return (
     <div className="main">
+      <video src={videoBg} autoPlay loop muted />
       <div className="content">IKOSEVETA</div>
     </div>
   );
