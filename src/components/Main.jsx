288250b0d4a5cf170a49fd97fd1b4734d3830660
mcgidoug <mcgidoug@gmail.com>
2023-01-24 15:31:11 -0600

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="content">IKOSEVETA</div>
+    </div>
+  );
+};
+
+export default Main;
